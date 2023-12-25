06f22b697315421b0e0f88b6bc9a6aa48368b121
AlexeyAleksandrovichJ <143766479+AlexeyAleksandrovichJ@users.noreply.github.com>
2023-12-25 11:18:18 +0300

    sf

---
+++ multi_before_and_after_negative.h
@@ -0,0 +1,5 @@
+#pragma once
+#include "index_first_negative.h"
+#include "index_last_negative.h"
+
+void multi_before_and_after_negative(int arr[], int n);
---
+++ multi_between_negative.h
@@ -0,0 +1,5 @@
+#pragma once
+#include "index_first_negative.h"
+#include "index_last_negative.h"
+
+void multi_between_negative(int arr[], int n);
